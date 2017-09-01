07377f056df7f611af7a7c03dbbe2fa2cf6f9626
Boud <maxime.boudreault.0@gmail.com>
2017-08-31 23:16:52 -0400

    WIP: Basic usage of wheel colliders with unfriendly user controls.

---
+++ Assets/Motor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour
+{
+    public enum Direction
+    {
+        LEFT,
+        RIGHT,
+        STRAIGHT
+    }
+
+    [SerializeField] WheelCollider[] wheels;
+    [SerializeField] float power = 20f;
+    [SerializeField] float steering = 30f;
+    [SerializeField] float brake = 2f;
+
+    public void Steer(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.STRAIGHT:
+                Steer(0f);
+                break;
+            case Direction.LEFT:
+                Steer(-steering);
+                break;
+            case Direction.RIGHT:
+                Steer(steering);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Steer(float angle)
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.steerAngle = angle;
+        } 
+    }
+
+    public void TurnRight()
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.steerAngle = steering;
+        }
+    }
+
+    public void Accelerate()
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.brakeTorque = 0;
+            wheel.motorTorque += power;
+        }
+    }
+
+    public void Brake()
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.motorTorque = 0;
+            wheel.brakeTorque += brake;
+        }
+    }
+}
---
+++ Assets/VehicleCommands.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleCommands : MonoBehaviour
+{
+    [SerializeField] Motor motor;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            motor.Accelerate();
+        }
+        else
+        {
+            motor.Brake();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+            motor.Steer(Motor.Direction.LEFT);
+        else if (Input.GetKeyDown(KeyCode.D))
+            motor.Steer(Motor.Direction.RIGHT);
+        else if (Input.GetKeyDown(KeyCode.W))
+            motor.Steer(Motor.Direction.STRAIGHT);
+    }
+}
