6447edad5a0f98a1416a0e07e997370024d41712
Boud <maxime.boudreault.0@gmail.com>
2017-09-21 11:32:17 -0400

    Improved cloth demo, character controller demo (WIP), physics material.

---
+++ Assets/CharacterCommand.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CharacterCommand : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 2;
+    [SerializeField] private float turnSpeed = 200;
+    [SerializeField] private float jumpForce = 4;
+    [SerializeField] private Animator animator;
+    [SerializeField] private CharacterController character;
+
+    private float m_currentV = 0;
+    private float m_currentH = 0;
+
+    private readonly float m_interpolation = 10;
+    private readonly float m_walkScale = 0.33f;
+    private readonly float m_backwardsWalkScale = 0.16f;
+    private readonly float m_backwardRunScale = 0.66f;
+
+    private bool wasGrounded;
+    private Vector3 currentDirection = Vector3.zero;
+
+    private float jumpTimeStamp = 0;
+    private float minJumpInterval = 0.25f;
+
+    private List<Collider> m_collisions = new List<Collider>();
+
+    void Update()
+    {
+        animator.SetBool("Grounded", character.isGrounded);
+        DirectUpdate();
+        wasGrounded = character.isGrounded;
+    }
+
+    private void DirectUpdate()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        Transform camera = Camera.main.transform;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            v *= m_walkScale;
+            h *= m_walkScale;
+        }
+
+        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+
+        Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
+
+        float directionLength = direction.magnitude;
+        direction.y = 0;
+        direction = direction.normalized * directionLength;
+
+        if (direction != Vector3.zero)
+        {
+            currentDirection = Vector3.Slerp(currentDirection, direction, Time.deltaTime * m_interpolation);
+
+            transform.rotation = Quaternion.LookRotation(currentDirection);
+            character.SimpleMove(currentDirection * moveSpeed * Time.deltaTime);
+
+            animator.SetFloat("MoveSpeed", direction.magnitude);
+        }
+
+        JumpingAndLanding();
+    }
+
+    private void JumpingAndLanding()
+    {
+        bool jumpCooldownOver = (Time.time - jumpTimeStamp) >= minJumpInterval;
+
+        if (jumpCooldownOver && character.isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            jumpTimeStamp = Time.time;
+            character.SimpleMove(Vector3.up * jumpForce);
+        }
+
+        if (!wasGrounded && character.isGrounded)
+        {
+            animator.SetTrigger("Land");
+        }
+
+        if (!character.isGrounded && wasGrounded)
+        {
+            animator.SetTrigger("Jump");
+        }
+    }
+}
