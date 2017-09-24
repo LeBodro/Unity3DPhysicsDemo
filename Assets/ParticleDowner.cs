19fb8b7877fa693b623342a5d06fe9125346b9bb
Boud <maxime.boudreault.0@gmail.com>
2017-09-24 11:14:58 -0400

    Add bomblet particles that push floor down.

---
+++ Assets/ParticleDowner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ParticleDowner : MonoBehaviour
+{
+    [SerializeField] float downBump = 0.1f;
+
+    void OnParticleCollision(GameObject other)
+    {
+        var newPos = other.transform.position;
+        newPos.y -= downBump;
+        other.transform.position = newPos;
+    }
+}
