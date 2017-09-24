0a6ad380f2eab1f05292ce3f35846a24cf26dc36
Boud <maxime.boudreault.0@gmail.com>
2017-09-24 17:33:38 -0400

    Add correct floating platform demo.

---
+++ Assets/Kinematic/PlatformRigidbodyMover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlatformRigidbodyMover : MonoBehaviour
+{
+    [SerializeField] float rotationPerSecond = -15f;
+
+    Rigidbody ownBody;
+
+    void Start()
+    {
+        ownBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        var frameRotation = rotationPerSecond * Time.fixedDeltaTime;
+        Transformation movement = new Rotation(Vector3.up, frameRotation * Mathf.Deg2Rad);
+
+        ownBody.MovePosition(movement.ApplyTo(transform.position));
+        ownBody.MoveRotation(Quaternion.Euler(transform.eulerAngles - frameRotation * Vector3.up));
+    }
+}
