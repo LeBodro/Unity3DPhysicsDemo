33427b18acff9d90a32794607580a67e5cd4121c
Boud <maxime.boudreault.0@gmail.com>
2017-09-25 10:00:44 -0400

    Add center of mass component and improve capsule vs cylinder demo.

---
+++ Assets/Kinematic/CenterOfMass.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CenterOfMass : MonoBehaviour
+{
+    [SerializeField] Vector3 centerOfMass;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().centerOfMass = centerOfMass;
+    }
+}
