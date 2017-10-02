acde7cfef48874d195d8a0f43eaeddf247c38835
Boud <maxime.boudreault.0@gmail.com>
2017-10-02 12:11:59 -0400

    Add stuff controller and improve scene navigation

--- Assets/DemoScenes/SceneNavigator.cs
+++ Assets/DemoScenes/SceneNavigator.cs
@@ -1,34 +1,54 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class SceneNavigator : MonoBehaviour
 {
     int currentSceneIndex;
 
-    int sceneCount { get { return SceneManager.sceneCount; } }
+    int sceneCount { get { return SceneManager.sceneCountInBuildSettings; } }
+
+    float changeCooldown = 0.1f;
+    float timeLeftForChange;
+
+    IDictionary<string, bool> downAxes = new Dictionary<string, bool>();
 
     void Awake()
     {
-        DontDestroyOnLoad(this);
+        if (FindObjectsOfType<SceneNavigator>().Length == 1)
+        {
+            DontDestroyOnLoad(this);
+            downAxes.Add("Next", Input.GetAxis("Next") > 0.4f);
+            downAxes.Add("Previous", Input.GetAxis("Previous") > 0.4f);
+        }
+        else
+        {
+            DestroyObject(gameObject);
+        }
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonUp(0))
+        timeLeftForChange -= Time.deltaTime;
+        if (timeLeftForChange > 0)
+            return;
+        
+        if (GetAxisDown("Next"))
         {
             LoadNextScene();
         }
-        else if (Input.GetMouseButtonDown(1))
+        else if (GetAxisDown("Previous"))
         {
             LoadPreviousScene();
         }
+
     }
 
     void LoadNextScene()
     {
         currentSceneIndex++;
         currentSceneIndex %= sceneCount;
-
+        timeLeftForChange = changeCooldown;
         SceneManager.LoadScene(currentSceneIndex);
     }
 
@@ -38,6 +58,16 @@ public class SceneNavigator : MonoBehaviour
         if (currentSceneIndex < 0)
             currentSceneIndex = sceneCount - 1;
 
+        timeLeftForChange = changeCooldown;
         SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    bool GetAxisDown(string axis)
+    {
+        bool currentlyDown = Input.GetAxis(axis) > 0.4f;
+        bool justDown = !downAxes[axis] && currentlyDown;
+        Debug.Log(currentlyDown + " " + downAxes[axis] + " " + justDown);
+        downAxes[axis] = currentlyDown;
+        return justDown;
     }
 }
---
+++ Assets/Main/StuffController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class StuffController : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+
+    Rigidbody ownBody;
+
+    void Start()
+    {
+        ownBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        Vector3 velocity = new Vector3(h, v, 0).normalized * speed * Time.fixedDeltaTime;
+        ownBody.MovePosition(transform.position + velocity);
+    }
+}
