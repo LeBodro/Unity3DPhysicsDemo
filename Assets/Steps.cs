bf794ef01d9746ad218fd6d7a14c28039503ec29
Boud <maxime.boudreault.0@gmail.com>
2017-10-02 15:58:30 -0400

    Add step by step in some scenes

---
+++ Assets/Steps.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Steps : MonoBehaviour
+{
+    [SerializeField] GameObject[] steps;
+
+    int currentStep;
+    bool buttonDown;
+
+    void Update()
+    {
+        if (GetAxisDown("Step"))
+        {
+            Next();
+        }
+    }
+
+    void Next()
+    {
+        steps[currentStep].SetActive(false);
+        currentStep++;
+        currentStep %= steps.Length;
+        steps[currentStep].SetActive(true);
+    }
+
+    bool GetAxisDown(string axis)
+    {
+        bool currentlyDown = Input.GetAxis(axis) > 0.4f;
+        bool justDown = !buttonDown && currentlyDown;
+        buttonDown = currentlyDown;
+        return justDown;
+    }
+}
