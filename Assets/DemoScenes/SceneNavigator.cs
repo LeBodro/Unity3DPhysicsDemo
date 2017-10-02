b599e57aea85decc0f44e5ec978bf7804ff93c6a
Boud <maxime.boudreault.0@gmail.com>
2017-10-02 10:45:29 -0400

    Add separate demo scenes for standalone build demo

---
+++ Assets/DemoScenes/SceneNavigator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigator : MonoBehaviour
+{
+    int currentSceneIndex;
+
+    int sceneCount { get { return SceneManager.sceneCount; } }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            LoadNextScene();
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            LoadPreviousScene();
+        }
+    }
+
+    void LoadNextScene()
+    {
+        currentSceneIndex++;
+        currentSceneIndex %= sceneCount;
+
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadPreviousScene()
+    {
+        currentSceneIndex--;
+        if (currentSceneIndex < 0)
+            currentSceneIndex = sceneCount - 1;
+
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
